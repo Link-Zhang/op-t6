6b7af6e41dd3aa7808ee9ae87ffc0ed2a7a4a25b
Link Zhang <Link.Zhang.0@gmail.com>
2025-12-25 17:28:47 +0800

    v0.0.2: add luci-theme-argon

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,39 +10,24 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify hostname
+sed -i 's/OpenWrt/T6/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# Modify hostname
-#sed -i 's/LEDE/Pi5Plus/g' package/base-files/luci2/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 # Modify Luci Login Page
-#sed -i '23,25d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
+sed -i '24,26d' package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
 
 # Modify Luci Page
-#sed -i '23,26d' feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
+sed -i '23,33d' package/luci-theme-argon/luasrc/view/themes/argon/footer.htm
 
-# Change ash to bash
-#sed -i 's|/bin/ash$|/bin/bash|g' package/base-files/files/etc/passwd
-
-# Change kernel to 6.12
-#sed -i 's|6.12|6.12|g' target/linux/rockchip/Makefile
-#sed -i 's|6.6|6.12|g' target/linux/rockchip/Makefile
+# Change ash to zsh
+sed -i 's|/bin/ash|/bin/zsh|g' package/base-files/files/etc/passwd
 
 # Change ttyd from services to system
-#sed -i 's|/services/|/system/|g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+sed -i 's|/services/|/system/|g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
 
 # Change samba4 from services to nas
-#sed -i 's|/services/|/nas/|g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
+sed -i 's|/services/|/nas/|g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
 
-# Change theme depends to remove luci-theme-bootstrap
-#sed -i 's|+luci-theme-bootstrap|+luci-theme-argon|g' feeds/luci/collections/luci-light/Makefile
